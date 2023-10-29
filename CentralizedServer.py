a8027223f8a2eb89a5cd11f5f895e7fb1df3f9ac
Bâng Đỗ Văn <bang.dovan38@gmail.com>
2023-10-29 11:33:35 +0700

    wrong behavior everwhere

--- CentralizedServer.py
+++ CentralizedServer.py
@@ -20,71 +20,80 @@ class CentralizedServer(Thread):
         self.server_socket.bind((self.host, self.port))
         self.server_socket.listen(maximum_client_number)
         self.files = []
-        self.clientHostName = []
-        self.fileMetaData = ['peerId', 'hostName', 'fileName', 'dateAdded']
+        self.clientHost = []
+        self.fileMetaData = ['host_name', 'file_name', 'date_added']
         print('Connect server at', self.host, 'with port is', self.port)
     
     def run(self):
-        while True:
-            client, client_addr = self.server_socket.accept()
-            print('Connected to', client_addr[0], 'port at', client_addr[1])
-            
-            # request = [discover <hostname>, 
+        while True:            
+            # request = [
+            #            register <hostname> <password>
+            #            login <hostname> <password>
+            #            discover <hostname>, 
             #            ping <hostname>,
             #            fetch <fname>, 
             #            publish <fname> <lname>,
             #            ]            
+            
+            client, client_addr = self.server_socket.accept()
+            print('Connected to', client_addr[0], 'port at', client_addr[1])
+            
             request = pickle.loads(client.recv(Environment.PACKET_SIZE))
             # => [word_1, word_2,....]
             message_type = request[0]
-            host_name = request[1]
-            
-            if message_type == 'discover':
+
+            is_logged_in = False
+            if message_type == 'register':
+                print('Client', client_addr[0], 'want to register to use the server')
+                self.semaphore.acquire()
+                client_message = self.client_register(request[1], request[2], client_addr[0], client_addr[1])
+                client.send(pickle.dumps(client_message))
+                self.semaphore.release()
+            elif message_type == 'login':
+                print('Client', client_addr[0], 'is logging in to the server')
+                self.semaphore.acquire()
+                client_message = self.client_register(request[1], request[2])
+                client.send(pickle.dumps(client_message))
+                self.semaphore.release()
+            elif message_type == 'discover':
                 print('Client', client_addr[1], 'requests to list files of', request[1])
                 self.semaphore.acquire()
                 try:
-                    list_of_files = pickle.dumps(self.list_of_files(host_name))
+                    list_of_files = pickle.dumps(self.list_of_files(request[1]))
                     client.send(list_of_files)
                 except FileNotFoundError:
                     client.send('The server is not found your requested hostname')
                 self.semaphore.release()
             elif message_type == 'publish':
-                print('Client with', client_addr, 'is want to share file')
+                print('Client with', client_addr[1], 'is want to share file')
                 self.semaphore.acquire()
-                
                 # Check duplicate file name in server
                 peer_file_info = request[2]
                 message_to_client = "File Registered Successfully."
                 for index_file in self.files:
-                    if index_file['fileName'] == peer_file_info:
+                    if index_file['file_name'] == peer_file_info:
                         peer_file_info = request[2].split('.')
-                        print(peer_file_info)
                         peer_file_info = peer_file_info[0] + ' (1).' + peer_file_info[1]
                         message_to_client += '\n Duplicate file name in server directory. \n Name is changed to ' + peer_file_info
                         break
-                self.files.insert(0, \
-                    dict(zip(self.fileMetaData, [str(self.host), host_name, peer_file_info, str(datetime.now())])))
-                print(self.files)
-                client.send(bytes(message_to_client, 'utf-8'))
+                self.files.insert(0, dict(zip(self.fileMetaData, [str(self.host), peer_file_info, str(datetime.now())])))
+                client.send(pickle.dumps(message_to_client))
                 self.semaphore.release()
             elif message_type == 'ping':
                 self.semaphore.acquire()
-                isHostLive = self.ping(host_name)
-                print("The host is online") if isHostLive else print("The host is offline")
+                host_addr = self.search_client_host_addr(request[1])
+                client.send(pickle.dumps(host_addr))
                 self.semaphore.release()
             else:
                 break
                 #print('Wrong command line')
                 
-                
-    def ping(self, host_name):
-        # Option for the number of packets as a function of
-        param = '-n' if platform.system().lower()=='windows' else '-c'
-
-        # Building the command. Ex: "ping -c 1 google.com"
-        command = ['ping', param, '1', host_name]
-
-        return subprocess.call(command) == 0
+    def search_client_host_addr(self, host_name):
+        # return peer_addr to peer want to ping
+        for client_host in self.clientHost:
+            if client_host['host_name'] == host_name:
+                return client_host['host_addr']
+        return 'NOT_FOUND' 
                         
     
     def list_of_files(self, host_name):
@@ -92,15 +101,37 @@ class CentralizedServer(Thread):
         if host_name == 'localhost':
             return self.files, self.fileMetaData
         else:
-            is_client_host_in_server = [host_name == client_host_name for client_host_name in self.clientHostName]
+            is_client_host_in_server = [host_name == client_host_name['host_name'] for client_host_name in self.clientHost]
             if is_client_host_in_server:
                 for file in self.files:
                     if file['host_name'] == host_name:
-                        file_metadata = [file['peer_id'], file['host_name'], \
-                                        file['file_name'], file['date-added']]
-                        file_lists.push(dict(zip(self.fileMetaData, file_metadata)))
+                        file_metadata = [file['host_name'], file['file_name'], file['date-added']]
+                        file_lists.append(dict(zip(file_metadata, self.fileMetaData)))
                 return file_lists
             raise FileNotFoundError('Hostname is not found')
+    
+    def client_register(self, client_host_name, client_password, client_addr, client_port):
+        # Check the client host name list in the server
+        # If there is a duplicate host name, the server refuses the register request, return 'DUP'
+        # Else, add client host name and its password to the server, return 'OK'
+        
+        # SHOULD BE DONE: encrypt the password before client send and before store to the server
+        # Refer assymetric encryption algorithm
+        
+        for host_name in self.clientHost:
+            if client_host_name == host_name['host_name']:
+                return 'DUP'
+
+        self.clientHost.append(dict(zip(\
+                                            ('host_name', 'host_password', 'host_addr', 'host_port'), \
+                                            (client_host_name, str(client_password), client_addr, client_port)\
+                                            )\
+                                        )\
+                                   )
+        return 'OK'
+    
+    def client_login(self, client_host_name, client_password):
+        pass
     
 print("Welcome. Server is about to go live")
 server = CentralizedServer(2)
--- Client.py
+++ Client.py
@@ -13,20 +13,29 @@ class PeerManager:
     def __init__(self):
         self.file_path = ''
         self.peer_port = Environment.SERVER_PORT
+        self.host_name = ''
+        self.host_password = ''
         print("Started P2P file sharing system")
+        print("""
+Command line support: \n
+register <hostname> <password>
+login <hostname> <password>
+publish <file_path_at_peer> <file_name_at_server>
+fetch <file_name> 
+discover <host_name> 
+ping <host_name>  """)
         
     def P2PServer_start(self):
         while True:
-            request = input("""
-Command line support \n
-publish <file_path_at_peer> <file_name_at_server> \n
-fetch <file_name> \n
-discover <host_name> \n
-ping <host_name> \n """)
+            request = input('Input your request: ')
             request = request.split()
             message_type = request[0]
             
-            if message_type == 'publish':
+            if message_type == 'register':
+                self.register(request[1], request[2])
+            elif message_type == 'login':
+                pass
+            elif message_type == 'publish':
                 self.peer_port = Environment.SERVER_PORT
                 self.file_path = request[1]
                 self.publish()
@@ -35,69 +44,116 @@ ping <host_name> \n """)
             elif message_type == 'discover':
                 self.discover(request[1])
             elif message_type == 'ping':
-                self.ping(request[1])
+                print(self.ping(socket.gethostbyname(request[1])))
             else:
+                print('Wrong command line. Exited!')
                 break
     
+    def register(self, host_name, host_password):
+        client_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_connection.connect((Environment.SERVER_HOST_NAME, Environment.SERVER_PORT))
+        
+        registered_stream = ['register', host_name, host_password]
+        data_stream = pickle.dumps(registered_stream)
+        client_connection.send(data_stream)
+        
+        client_state = client_connection.recv(Environment.PACKET_SIZE)
+        client_state = pickle.loads(client_state)
+        if client_state == 'OK':
+            self.host_name = host_name
+            self.host_password = host_password
+            print('Your registration is success!')
+        else:
+            print('Your chosen host name was in the server! Please choose another name!')
+    
+    def login(self, host_name, host_password):
+        # check host_name 
+        client_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_connection.connect((Environment.SERVER_HOST_NAME, Environment.SERVER_PORT))
+        
+        login_stream = ['login', host_name, host_password]
+        data_stream = pickle.dumps(login_stream)
+        client_connection.send(data_stream)
+        
+        client_state = client_connection.recv(Environment.PACKET_SIZE)
+        client_state = pickle.loads(client_state)
+        if client_state == 'OK':
+            print('Login success!')
+            return True
+        elif client_state == 'WRONG_PASSWORD':
+            print('Login fail because of wrong password!')
+        else:
+            print('Host name is not found in the server')
+        return False
+    
     def publish(self):
-        peer_connection = socket.socket()
-        peer_connection.connect(('localhost', self.peer_port))
+        client_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_connection.connect(('localhost', self.peer_port))
         published_stream = ['publish', self.file_path, self.peer_port]
         data_stream = pickle.dumps(published_stream)
-        peer_connection.send(data_stream)
-        peer_state = peer_connection.recv(Environment.PACKET_SIZE)
-        print(peer_state.decode())
-        peer_connection.close()
+        client_connection.send(data_stream)
+        client_state = client_connection.recv(Environment.PACKET_SIZE)
+        client_state = pickle.loads(client_state)
+        print(client_state)
+        client_connection.close()
 
     def discover(self, host_name = 'localhost'):
-        peer_connection = socket.socket()
-        peer_connection.connect((host_name, self.peer_port))
+        client_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_connection.connect((host_name, self.peer_port))
         discover_request = ['discover', host_name]
-        peer_connection.send(pickle.dumps(discover_request))
-        peer_state = pickle.loads(peer_connection.recv(Environment.PACKET_SIZE))
+        client_connection.send(pickle.dumps(discover_request))
+        client_state = pickle.loads(client_connection.recv(Environment.PACKET_SIZE))
         
-        peer_files = peer_state[0]
-        peer_file_metadata = peer_state[1]
+        peer_files = client_state[0]
+        peer_file_metadata = client_state[1]
         if len(peer_files) == 0:
             print('The host doesn\'t has any files')
         else:
-            print('Peer_Port  Host_Name  File_Name    Date_Added')
+            print('Host_Name  File_Name    Date_Added')
             [print(file[peer_file_metadata[0]], ' ', \
                    file[peer_file_metadata[1]], ' ', \
-                   file[peer_file_metadata[2]], ' ', \
-                   file[peer_file_metadata[3]]       \
+                   file[peer_file_metadata[2]]       \
                    ) 
                 for file in peer_files
             ]
         
-        peer_connection.close()
+        client_connection.close()
 
     def ping(self, host_name):
+        client_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_connection.connect((host_name, self.peer_port))
+        ping_request = ['ping', host_name]
+        client_connection.send(pickle.dumps(ping_request))
+        client_state = pickle.loads(client_connection.recv(Environment.PACKET_SIZE))
+        
+        # if (host_name != 'localhost') and (client_state == 'NOT_FOUND'):
+        #     return 'The host name is not found in the server'        
+        
         param = '-n' if platform.system().lower()=='windows' else '-c'
 
-        # Building the command. Ex: "ping -c 1 google.com"
-        command = ['ping', param, '1', host_name]
+        # Building the command. Ex: "ping -c 4 google.com"
+        command = ['ping', param, '4', host_name]
 
         return subprocess.call(command) == 0
     
     def fetch(self, file_name):
-        peer_connection = socket.socket()
-        peer_connection.connect(('localhost', self.peer_port))
+        client_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_connection.connect(('localhost', self.peer_port))
         fetched_stream = ['fetch', file_name]
         data_stream = pickle.dumps(fetched_stream)
-        peer_connection.send(data_stream)
+        client_connection.send(data_stream)
         
         peer_file_path = os.path.join(os.getcwd(), 'SharedFiles/Downloads')
         
         with open(os.path.join(peer_file_path, file_name), 'wb') as download_file:
             while True:
-                file_stream = peer_connection.recv(Environment.PACKET_SIZE)
+                file_stream = client_connection.recv(Environment.PACKET_SIZE)
                 print(file_stream.decode())
                 if not file_stream:
                     download_file.close()
                     break
                 download_file.write(file_name)
-        peer_connection.close()
+        client_connection.close()
         print('The file is downloaded in your repository')
 
 peer = PeerManager()
